12983fa70231dda52125e6c49e2d55b597f23afe
SRB <souhardaroybarman05@gmail.com>
2025-06-16 21:06:25 +0530

    Refactor project controller and routes for improved structure and readability

--- controllers/project.js
+++ controllers/project.js
@@ -1,125 +1,171 @@
-import Workspace from "../models/workspace.js";
 import Project from "../models/project.js";
-import Task from "../models/task.js"
+import Task from "../models/task.js";
+import Workspace from "../models/workspace.js";
 
 const createProject = async (req, res) => {
-  try {
-    const {workspaceId} = req.params;
-    const {title, description, status,startDate,dueDate,tags,members} = req.body;
-    const workspace = await Workspace.findById(workspaceId);
+	try {
+		const { workspaceId } = req.params;
+		const { title, description, status, startDate, dueDate, tags, members } =
+			req.body;
+		const workspace = await Workspace.findById(workspaceId);
 
-    if(!workspace){
-      return res.status(500).json({message:"Workspace not found"});
-    }
+		if (!workspace) {
+			return res.status(500).json({ message: "Workspace not found" });
+		}
 
-    const isMember = workspace.members.some((member) => member.user.toString() === req.user._id.toString());
+		const isMember = workspace.members.some(
+			(member) => member.user.toString() === req.user._id.toString()
+		);
 
-    if(!isMember){
-      return res.status(401).json({
-        message:"You are not a member of this workspace"
-      })
-    }
+		if (!isMember) {
+			return res.status(401).json({
+				message: "You are not a member of this workspace",
+			});
+		}
 
-    const tagArray = tags ? tags.split(",") : [];
+		const tagArray = tags ? tags.split(",") : [];
 
-    const newProject =await Project.create({
-      title,
-      description,
-      status,
-      startDate,
-      dueDate,
-      tags:tagArray,
-      workspace:workspaceId,
-      members,
-      createdBy:req.user._id,
-    });
+		const newProject = await Project.create({
+			title,
+			description,
+			status,
+			startDate,
+			dueDate,
+			tags: tagArray,
+			workspace: workspaceId,
+			members,
+			createdBy: req.user._id,
+		});
 
-    workspace.projects.push(newProject._id);
-    await workspace.save();
+		workspace.projects.push(newProject._id);
+		await workspace.save();
 
-    return res.status(201).json({
-      message:"Project Created Successfully",
-      project:newProject,
-      workspace:workspaceId
-    })
-
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      message:"Internal Server Error",
-    })
-  }
-}
+		return res.status(201).json({
+			message: "Project Created Successfully",
+			project: newProject,
+			workspace: workspaceId,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			message: "Internal Server Error",
+		});
+	}
+};
 
 const getProjectDetails = async (req, res) => {
-  try {
-    const {projectId} = req.params;
+	try {
+		const { projectId } = req.params;
 
-    const project = await Project.findById(projectId);
-    
-    if(!project) {
-      return res.status(404).json({
-        message:"Project not found",
-      });
-    }
+		const project = await Project.findById(projectId);
 
-    const isMember = project.members.some(
-      (member) => member.user.toString() === req.user._id.toString()
-    );
+		if (!project) {
+			return res.status(404).json({
+				message: "Project not found",
+			});
+		}
 
-    if(!isMember){
-      return res.status(403).json({
-        message:"You are not a member of this project",
-      });
-    }
+		const isMember = project.members.some(
+			(member) => member.user.toString() === req.user._id.toString()
+		);
 
-    return res.status(200).json(project);
+		if (!isMember) {
+			return res.status(403).json({
+				message: "You are not a member of this project",
+			});
+		}
 
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      message:"Internal Server Error",
-    });
-  }
+		return res.status(200).json(project);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			message: "Internal Server Error",
+		});
+	}
 };
 
 const getProjectTasks = async (req, res) => {
-  try {
-    const {projectId} = req.params;
-    const project = await Project.findById(projectId).populate("members.user");
+	try {
+		const { projectId } = req.params;
+		const project = await Project.findById(projectId).populate("members.user");
 
-    if(!project){
-      return res.status(404).json({
-        message:"Project not found"
-      });
-    }
+		if (!project) {
+			return res.status(404).json({
+				message: "Project not found",
+			});
+		}
 
-    const isMember = project.members.some(
-      (member) => member.user._id.toString() === req.user._id.toString()
-    );
+		const isMember = project.members.some(
+			(member) => member.user._id.toString() === req.user._id.toString()
+		);
 
-    if(!isMember){
-      return res.status(403).json({
-        message:"You are not a member of this project",
-      });
-    }
+		if (!isMember) {
+			return res.status(403).json({
+				message: "You are not a member of this project",
+			});
+		}
 
-    const tasks = await Task.find({
-      project:projectId,
-      isArchived:false
-    }).populate("assignees","name profilePicture" ).sort({createdAt:-1});
+		const tasks = await Task.find({
+			project: projectId,
+			isArchived: false,
+		})
+			.populate("assignees", "name profilePicture")
+			.sort({ createdAt: -1 });
 
-    return res.status(200).json({
-      project,
-      tasks,
-    });
+		return res.status(200).json({
+			project,
+			tasks,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			message: "Internal Server Error",
+		});
+	}
+};
 
-  } catch (error) {
-    console.log(error);
-     return res.status(500).json({
-      message:"Internal Server Error",
-    });
-  }
-}
+const updateProjectStatus = async (req, res) => {
+	try {
+		const { projectId } = req.params;
+		const { status } = req.body;
 
-export {createProject,getProjectDetails,getProjectTasks}; 
+		const project = await Project.findById(projectId);
+
+		if (!project) {
+			return res.status(500).json({
+				message: "Project not found",
+			});
+		}
+
+		const isMember = project.members.some(
+			(member) => member.user.toString() === req.user._id.toString()
+		);
+
+		if (!isMember) {
+			return res.status(500).json({
+				message: "Not a member of the project",
+			});
+		}
+
+		// Actually update the task
+		project.status = status;
+		await project.save();
+
+		return res.status(200).json({
+			message: "Task title updated successfully",
+			project,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			message: "Internal Server Error",
+		});
+	}
+};
+
+export {
+	createProject,
+	getProjectDetails,
+	getProjectTasks,
+	updateProjectStatus,
+};
--- routes/project.js
+++ routes/project.js
@@ -1,43 +1,58 @@
 import express from "express";
-import {validateRequest} from 'zod-express-middleware';
+import { z } from "zod";
+import { validateRequest } from "zod-express-middleware";
+import {
+	createProject,
+	getProjectDetails,
+	getProjectTasks,
+	updateProjectStatus,
+} from "../controllers/project.js";
 import { projectSchema } from "../libs/validate-schema.js";
-import {authMiddleware} from "../middleware/auth-middleware.js";
-import { createProject ,getProjectDetails,getProjectTasks} from "../controllers/project.js";
-import {z} from "zod";
-
+import { authMiddleware } from "../middleware/auth-middleware.js";
 
 const router = express.Router();
 
-router.post("/:workspaceId/create-project",
-  authMiddleware,
-  validateRequest({
-    params:z.object({
-      workspaceId:z.string(),
-    }),
-    body:projectSchema,
-  }),
-  createProject
+router.post(
+	"/:workspaceId/create-project",
+	authMiddleware,
+	validateRequest({
+		params: z.object({
+			workspaceId: z.string(),
+		}),
+		body: projectSchema,
+	}),
+	createProject
 );
 
 router.get(
-  "/:projectId",
-  authMiddleware,
-  validateRequest({
-    params:z.object({
-      projectId:z.string()
-    })
-  }),
-  getProjectDetails
+	"/:projectId",
+	authMiddleware,
+	validateRequest({
+		params: z.object({
+			projectId: z.string(),
+		}),
+	}),
+	getProjectDetails
 );
 
 router.get(
-  "/:projectId/tasks",
-  authMiddleware,
-  validateRequest({
-    params:z.object({
-      projectId:z.string()
-    })
-  }),
-  getProjectTasks
-)
+	"/:projectId/tasks",
+	authMiddleware,
+	validateRequest({
+		params: z.object({
+			projectId: z.string(),
+		}),
+	}),
+	getProjectTasks
+);
+
+router.put(
+	"/:projectId/status",
+	authMiddleware,
+	validateRequest({
+		params: z.object({ projectId: z.string() }),
+		body: z.object({ status: z.string() }),
+	}),
+	updateProjectStatus
+);
 export default router;
