b122046463174191d1df289b6b78c9b327364ec9
SRB <souhardaroybarman05@gmail.com>
2025-06-22 15:41:37 +0530

    chore: add @flydotio/dockerfile as a dev dependency

--- controllers/user.js
+++ controllers/user.js
@@ -35,7 +35,7 @@ export const uploadProfilePicture = async (req, res) => {
 	const user = await User.findById(req.user._id);
 	if (!user) return res.status(404).json({ message: "User not found" });
 
-	user.profilePicture = `https://flowstate-backend.onrender.com/profile-pictures/${req.file.filename}`;
+	user.profilePicture = ` https://backend-icy-frost-8085.fly.dev/profile-pictures/${req.file.filename}`;
 	await user.save();
 
 	res.status(200).json({
